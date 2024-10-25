1173fa75e1795895e4b8808127dae6a12ff27ba7
Greg JEFTIC <ajkula@gmail.com>
2024-10-26 00:45:56 +0200

    added demo page, README to served files

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,42 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'index.js',
+  external: ['three', 'gsap'],
   output: [
     {
       file: 'dist/domension.js',
       format: 'umd',
       name: 'DOMension',
+      globals: {
+        'three': 'THREE',
+        'gsap': 'gsap'
+      },
       sourcemap: true
     },
     {
       file: 'dist/domension.min.js',
       format: 'umd',
       name: 'DOMension',
+      globals: {
+        'three': 'THREE',
+        'gsap': 'gsap'
+      },
       plugins: [terser()],
       sourcemap: true
     }
   ],
   plugins: [
-    resolve()
+    resolve(),
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'README.md', dest: 'dist' },
+        { src: 'demo.html', dest: 'dist' },
+      ]
+    })
   ]
 };
